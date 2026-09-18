94998e03dfbf5e142adb24ee8b900145e44a2ab1
gabrielvilela-code <gabriel.vclima2008@gmail.com>
2026-09-18 12:46:13 -0300

    4

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,11 @@
 const botao= window.document.getElementById('menu')
 const menu = document.querySelector('ul.navegation')
+const fechar = document.getElementById('fechar')
 
 botao.addEventListener('click', function(){
     menu.classList.toggle('ativo');
+})
+
+fechar.addEventListener('click', function(){
+    menu.classList.remove('ativo');
 })
